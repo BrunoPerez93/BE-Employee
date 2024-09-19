dc6eea3bd1954bcf57a83fe2e1a84431039fb0b1
Bruno Perez <brunoperez24793@gmail.com>
2024-09-19 17:53:46 -0300

    cruds

--- obj/Debug/net8.0/React-ASPNet.AssemblyInfo.cs
+++ obj/Debug/net8.0/React-ASPNet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("React-ASPNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab139ff4602cd2a27ee3c3fce8a818546d474b43")]
 [assembly: System.Reflection.AssemblyProductAttribute("React-ASPNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("React-ASPNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
